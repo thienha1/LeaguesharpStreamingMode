31b6910f061724dc9c3480c94f3a3ed04d1d34ee
stefsot <stefsot@windowslive.com>
2014-11-19 13:37:18 +0200

    first commit

---
+++ LeaguesharpStreamingMode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace LeaguesharpStreamingMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("streaming enabled 2");
+            Assembly s = Assembly.Load(LeaguesharpStreamingMode.Properties.Resources.LeaguesharpStreamingModelib);
+            s.GetType("LeaguesharpStreamingModelib.StreamingMode").GetMethod("Enable").Invoke(null, null);
+            Console.WriteLine("streaming enabled 1");
+
+            AppDomain.CurrentDomain.DomainUnload += delegate
+            {
+                Assembly s2 = Assembly.Load(LeaguesharpStreamingMode.Properties.Resources.LeaguesharpStreamingModelib);
+                s2.GetType("LeaguesharpStreamingModelib.StreamingMode").GetMethod("Disable").Invoke(null, null);
+            };
+
+        }
+    }
+}
